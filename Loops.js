d0e8019cb6f297b9c750bdf9befd40831ea9c77e
swapna-2019 <48243271+swapna-2019@users.noreply.github.com>
2026-04-03 22:56:20 +0100

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,15 @@
+for(let i=10; i<=20; i++)
+    {
+if(i%2!=0){
+    console.log("print odd numbers " +i)
+    break;
+}
+}
+
+for(let i=10; i<=20; i++)
+    {
+if(i%2!=0){
+    console.log("print odd numbers " +i)
+    continue;
+}
+}
